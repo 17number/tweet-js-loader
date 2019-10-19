4f67215c5d8327a37cb5a50e987ecaf419d2b942
r17n <4716989+17number@users.noreply.github.com>
2019-10-19 10:09:09 +0900

    chore: convert to pug format

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  #app
+    img(alt="Vue logo" src="./assets/logo.png")
+    HelloWorld(msg="Welcome to Your Vue.js App")
 </template>
 
 <script>
